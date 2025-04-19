3619847c536ffd85cea6ae9d3f70f6a9f6ce0f26
tatimendez <159751352+tatimendez@users.noreply.github.com>
2025-04-19 18:24:35 -0300

    Update guiaejercicios1.sql

--- guiaejercicios1.sql
+++ guiaejercicios1.sql
@@ -1,18 +1,18 @@
---1. Cree una base de datos con los valores por defecto de crecimiento, ubicaciÛn de
+--1. Cree una base de datos con los valores por defecto de crecimiento, ubicaci√≥n de
 --archivos, etc. Llame a la misma con su nombre o el de su grupo
 
 create database grupo7
 
---2. Cree un esquema denominado ìddbbaî (por bases de datos aplicada). Todos los
---objetos que cree a partir de aquÌ deber·n pertenecer a este esquema o a otro seg˙n
+--2. Cree un esquema denominado ‚Äúddbba‚Äù (por bases de datos aplicada). Todos los
+--objetos que cree a partir de aqu√≠ deber√°n pertenecer a este esquema o a otro seg√∫n
 --corresponda. No debe usar el esquema default (dbo).
 
 create schema ddbba
 
---3. Cree una tabla llamada ìregistroî que tenga los siguientes campos: id entero
+--3. Cree una tabla llamada ‚Äúregistro‚Äù que tenga los siguientes campos: id entero
 --autoincremental como primary key; fecha y hora con valor default del momento de
---inserciÛn, texto con un tamaÒo m·ximo de 50 caracteres, modulo con un tamaÒo
---m·ximo de 10 caracteres. Esta tabla la emplear· como bit·cora (log) de las
+--inserci√≥n, texto con un tama√±o m√°ximo de 50 caracteres, modulo con un tama√±o
+--m√°ximo de 10 caracteres. Esta tabla la emplear√° como bit√°cora (log) de las
 --operaciones de los puntos siguientes.
 
 create table ddbba.registro (
@@ -31,8 +31,20 @@ create table ddbba.registro (
 --drop table ddbba.registro;
        -------------------------
 
---4. Cree un SP llamado ìinsertarLogî que reciba dos par·metros: modulo y texto. Si el
---modulo llega en blanco debe utilizar el texto ìN/Aî. Este SP insertar· registros en la
---tabla de bit·cora. A partir del siguiente punto, cada inserciÛn, borrado, modificaciÛn,
---debe crear un registro en la mencionada tabla. En ning˙n caso debe realizar
---INSERTs por fuera del SP insertarLog.create procedure ddbba.insertarLog (@texto char(50), @modulo char(10)) as	BEGIN--{	 if @modulo is null or @modulo =' '		insert into ddbba.registro (texto,modulo) values (@texto,'N/A')	else		insert into ddbba.registro (texto,modulo) values (@texto,@modulo)	END; --}--drop procedure ddbba.insertarLog;
+--4. Cree un SP llamado ‚ÄúinsertarLog‚Äù que reciba dos par√°metros: modulo y texto. Si el
+--modulo llega en blanco debe utilizar el texto ‚ÄúN/A‚Äù. Este SP insertar√° registros en la
+--tabla de bit√°cora. A partir del siguiente punto, cada inserci√≥n, borrado, modificaci√≥n,
+--debe crear un registro en la mencionada tabla. En ning√∫n caso debe realizar
+--INSERTs por fuera del SP insertarLog.
+
+create procedure ddbba.insertarLog (@texto char(50), @modulo char(10)) as
+	BEGIN--{
+	 if @modulo is null or @modulo =' '
+		insert into ddbba.registro (texto,modulo) values (@texto,'N/A')
+	else
+		insert into ddbba.registro (texto,modulo) values (@texto,@modulo)
+	END; --}
+
+--drop procedure ddbba.insertarLog;
+
+
